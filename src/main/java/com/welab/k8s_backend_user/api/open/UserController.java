aa8edffbede222355a26f5b923003c62a342fd30
sky121016 <sky121016@naver.com>
2025-06-11 14:41:41 +0900

    추가

--- src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
+++ src/main/java/com/welab/k8s_backend_user/api/open/UserController.java
@@ -4,14 +4,14 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import com.welab.k8s_backend_user.common.dto.ApiResponseDto;
 
 @Slf4j
 @RestController
-@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)
-@RequiredArgsConstructor
+@RequestMapping(value = "/api/user/v1", produces = MediaType.APPLICATION_JSON_VALUE)@RequiredArgsConstructor
 public class UserController {
-    @GetMapping(value = "/test")
-    public String test() {
-        return "Hello Kubernetes";
+    @GetMapping(value = "/hello")
+    public ApiResponseDto<String> hello() {
+        return ApiResponseDto.createOk("웰컴 투 백엔드 유저");
     }
 }
