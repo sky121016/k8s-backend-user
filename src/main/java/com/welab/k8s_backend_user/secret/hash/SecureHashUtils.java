bcf651d0108798d81efdfd8f38537175869e37f8
sky121016 <sky121016@naver.com>
2025-06-12 14:38:26 +0900

    kafka 연동

---
+++ src/main/java/com/welab/k8s_backend_user/secret/hash/SecureHashUtils.java
@@ -0,0 +1,14 @@
+package com.welab.k8s_backend_user.secret.hash;
+
+public class SecureHashUtils {
+    public static String hash(String password){
+        // TODO: message -> SHA-1 / SHA-256 해시 값으로 변환
+
+        return password;
+    }
+
+    public static boolean matches(String password, String hashedPassword){
+        String hashed = hash(password);
+        return hashed.equals(hashedPassword);
+    }
+}
