913cd185a3e4304c5db989d6b2185d31e4f51fb9
sky121016 <sky121016@naver.com>
2025-06-16 16:17:02 +0900

    update

---
+++ src/main/java/com/welab/k8s_backend_user/service/probe/ProbeService.java
@@ -0,0 +1,16 @@
+package com.welab.k8s_backend_user.service.probe;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class ProbeService {
+    public void validateLiveness(){
+
+    }
+
+    public void validateReadiness(){
+
+    }
+}
